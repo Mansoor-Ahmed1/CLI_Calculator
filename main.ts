85b9f2a0f5f33cb7a6e3fdb299dc6513232fc9bf
Mansoor Ahmed <mansoor.ahmed11521@gmail.com>
2024-05-11 11:42:03 +0500

    initial commit

---
+++ main.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let user_input = await inquirer.prompt(
+    [
+        {
+            "message": "Enter any Number",
+            "type": "number",
+            "name": "firstNum",
+        },
+        {
+            "message": "Enter another Number",
+            "type": "number",
+            "name": "secondNum",
+        },
+        {
+            "message": "What operation would you like to get performed between both numbers?",
+            "type": "list",
+            "name": "operation",
+            "choices": ["Addition", "Multiplication", "Subtraction", "Division"]
+        }
+    ]
+)
+
+if (user_input.operation == "Addition") {
+    let output:number = user_input.firstNum + user_input.secondNum
+    console.log("The Sum of both numbers would be " + output)
+}
+else if (user_input.operation == "Multiplication") {
+    let output:number = user_input.firstNum * user_input.secondNum
+    console.log("The Product of both numbers would be " + output)
+}
+else if (user_input.operation == "Subtraction") {
+    let output:number = user_input.firstNum - user_input.secondNum
+    console.log("The Difference of both numbers would be " + output)
+}
+else if (user_input.operation == "Division") {
+    let output:number = user_input.firstNum / user_input.secondNum
+    console.log("The Quotient of both numbers would be " + output)
+}
+else {
+    console.log("Error, Try again Please")
+}
